de26a71909a6a9e99196247a0da954ee0f2d05ac
Ori2846 <hdo2846@gmail.com>
2021-04-03 17:15:32 -0700

    Added Gun

---
+++ Assets/Bullet.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float speed = 20f;
+    public Rigidbody2D rb;
+    public int damage = 40;
+    // Start is called before the first frame update
+    void Start()
+    {
+      rb.velocity = transform.right * speed;
+      StartCoroutine(Timer());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void OnTriggerEnter2D(Collider2D hitInfo)
+    {
+        if (hitInfo.tag == "Bullet")
+        {
+
+        } else if (hitInfo.tag == "Wall")
+        {
+            Debug.Log(hitInfo.name);
+            Destroy(gameObject);
+        }
+    }
+    IEnumerator Timer()
+    {
+        yield return new WaitForSeconds(5);
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/Weapon.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    public GameObject BulletPrefab;
+    public Transform firePoint;
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        if  (Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+    }
+    void Shoot()
+    {
+        Instantiate(BulletPrefab, firePoint.position,firePoint.rotation);
+    }
+}
